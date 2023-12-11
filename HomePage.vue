89fce5be01eacc2ef5cdd96bd73beb9286881b25
johanna-keira <143952311+johanna-keira@users.noreply.github.com>
2023-12-11 09:51:33 -0600

    Create HomePage.vue
    Creación HomePage.vue Home

---
+++ HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Bienvenido a LentiTlax</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+
+</style>
+
