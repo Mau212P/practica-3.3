8f6ca3b22a606d47de993d3a69dec7395eff103c
johanna-keira <143952311+johanna-keira@users.noreply.github.com>
2023-12-11 09:50:23 -0600

    Create CategoriesPage.vue
    Creación CategoriesPage.vue Categorías

---
+++ CategoriesPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="categorias">
+    <h1>Categorías</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoriesPage'
+}
+</script>
+
+<style scoped>
+</style>
